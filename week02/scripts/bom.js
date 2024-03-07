efef8cbf60e87d42a42574ef51dfd5c48c32dd13
Nicolas Estoquin <nicolasabelestoquin@gmail.com>
2024-03-06 21:45:48 -0300

    DOM Manipulation finished

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,22 +2,20 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-
 button.addEventListener('click', () => {
     const deleteButton = document.createElement('button');
-    console.log('delete')
-    if(input.value != ''){
+    if((input.value != '') && (input.value != null) && (input.value != undefined)){
         const li = document.createElement('li');
         li.textContent = input.value;
         deleteButton.textContent = '❌';
         li.append(deleteButton);
         list.append(li);
         input.value = '';
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+            input.value = '';
+        })
     }
-    deleteButton.addEventListener('click', () => {
-        list.removeChild(li);
-        input.focus();
-    })
 })
-
 
