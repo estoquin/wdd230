6dd8a2bf81a05d25f11ca005e8fbeca9f72fdebe
Nicolas Estoquin <nicolasabelestoquin@gmail.com>
2024-03-14 14:16:30 -0300

    Temple section done

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,9 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
+const templetext = document.querySelector('#templetext')
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');	
+	templetext.classList.toggle('herotempletextmobile')
 });
