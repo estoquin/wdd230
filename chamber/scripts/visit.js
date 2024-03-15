1ca846559479ca40ba342d6c789be67c847e4df6
Nicolas Estoquin <nicolasabelestoquin@gmail.com>
2024-03-15 19:38:59 -0300

    Css hover finished, visit counter finished

--- chamber/scripts/visit.js
+++ chamber/scripts/visit.js
@@ -1,19 +1,17 @@
 
+const msToDays = 84600000;
 let currentVisit = Date.now()
 let lastVisit = localStorage.getItem('lastvisit')
 localStorage.setItem('lastvisit', currentVisit)
-const msToDays = 86400;
-
-// Operation
-console.log('Last Visit', parseInt(lastVisit))
-console.log('Current Visit: ', parseInt(currentVisit))
-let differenceBetweeDaysMiliseconds = parseInt(currentVisit) - parseInt(lastVisit) 
-console.log('differenceBetweeDaysMiliseconds: ', differenceBetweeDaysMiliseconds)
-let differenceDays = differenceBetweeDaysMiliseconds / msToDays
-console.log('Difference in days: ', parseInt(differenceDays))
-
-// Date
-let lastVisitDate = new Date(parseInt(lastVisit))
-let currentVisitDate = new Date(parseInt(currentVisit))
-console.log('Last Visit Date: ', lastVisitDate)
-console.log('Current Visit Date: ', currentVisitDate)
+let message = ''
+if ((lastVisit==null)||(lastVisit==undefined)){
+    message = 'Welcome! Let us know if you have any questions.'
+} else {
+    let differenceDays = parseInt((parseInt(currentVisit) - parseInt(lastVisit)) / msToDays)
+    if(differenceDays == 0) {
+        message="Back so soon! Awesome!"
+    } else {
+        message=`You last visited ${differenceDays} days ago`
+    }
+}
+document.querySelector('#textvisit').innerHTML = message
