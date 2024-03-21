6080bd3c4980e6ac69c3a20d265e4d9ee6ca70e1
Nicolas Estoquin <nicolasabelestoquin@gmail.com>
2024-03-21 16:45:36 -0300

    Pattern Fixed

--- scripts/form.js
+++ scripts/form.js
@@ -21,7 +21,6 @@ p2.addEventListener('focusout', checkpassword)
 // Range
 const rangevalue = document.getElementById("rangevalue");
 const range = document.getElementById("range");
-
 range.addEventListener('change', displayRatingValue);
 range.addEventListener('input', displayRatingValue);
 
