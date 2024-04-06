170f3e2b7f51541b73a136aee332d52e6725a6b1
Nicolas Estoquin <nicolasabelestoquin@gmail.com>
2024-04-06 15:22:44 -0300

    Organizing Code

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,7 +1,7 @@
+// Navigation
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('#navlinks');
 const navlink = document.querySelectorAll('.navlink');
-
 hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');	
 	navigation.classList.toggle('navmobile')
@@ -9,3 +9,10 @@ hamButton.addEventListener('click', () => {
 		nl.classList.toggle('navlinkmobile')
 	})
 });
+
+
+// Footer
+let currentYear = new Date().getFullYear();
+document.querySelector('#year').innerHTML=currentYear;
+let lastModificatedDate = document.lastModified;
+document.querySelector('#lastModified').innerHTML=`Last Modification ${lastModificatedDate}`;
